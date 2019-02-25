45b8e0932cbb254299eaad009307b203d6de7267
ShilovSetCorp <shilov-rom@mail.ru>
2019-02-25 14:20:22 +0300

    some refactor done part2

--- src/com/roman/shilov/hw5/Storage/Storage.java
+++ src/com/roman/shilov/hw5/Storage/Storage.java
@@ -5,7 +5,7 @@ import com.roman.shilov.hw5.countries.domain.Country;
 import com.roman.shilov.hw5.order.domain.Order;
 import com.roman.shilov.hw5.user.domain.User;
 
-public class Storage {
+public class storage {
     private static final int CAPACITY = 5;
     public static User[] users = new User[CAPACITY];
     public static Order[] orders = new Order[CAPACITY];
