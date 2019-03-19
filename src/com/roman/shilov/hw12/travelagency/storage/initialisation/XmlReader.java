839acfe56a11dc76ea5d35f0b687c13c151236e4
ShilovSetCorp <shilov-rom@mail.ru>
2019-03-19 14:18:21 +0300

    HW12 divided xml reader function

--- src/com/roman/shilov/hw12/travelagency/storage/initialisation/XmlReader.java
+++ src/com/roman/shilov/hw12/travelagency/storage/initialisation/XmlReader.java
@@ -32,13 +32,12 @@ public class XmlReader implements SourceReader<List<BaseCountry>> {
         NodeList countries = ((Element)root).getElementsByTagName("country");
 
         List<BaseCountry> parsedCountries = new ArrayList<>();
-
         if(countries.getLength() > 0){
             for (int i = 0; i < countries.getLength(); i++) {
 
                 Node xmlCountry = countries.item(i);
                 NodeList countryInnerTags = xmlCountry.getChildNodes();
-                NodeList coutryDescriptorTag = ((Element)countryInnerTags).getElementsByTagName("descriptor");//item(0);
+                NodeList coutryDescriptorTag = ((Element)countryInnerTags).getElementsByTagName("descriptor");
                 BaseCountry country = null;
                 //descriptor checking
                 if("hot".equals(coutryDescriptorTag.item(0).getTextContent())){
@@ -49,75 +48,75 @@ public class XmlReader implements SourceReader<List<BaseCountry>> {
 
                 if(country != null){
                     parsedCountries.add(country);
-
-                    for (int j = 1; j < countryInnerTags.getLength(); j++) {
-                        Node countryInner = countryInnerTags.item(j);
-
-                        switch (countryInner.getNodeName()){
-                            case "name": {
-                                country.setName(countryInner.getTextContent());
-                                break;
-                            }
-
-                            case "language": {
-                                country.setLanguage(countryInner.getTextContent());
-                                break;
-                            }
-
-                            case "telephoneCode": {
-                                country.setTelephoneCode(countryInner.getTextContent());
-                                break;
-                            }
-
-                            case "month": {
-                                if(country instanceof HotCountry){
-                                    ((HotCountry)country).setHottestMonth(Months.valueOf(countryInner.getTextContent()));
-                                }else {
-                                    ((ColdCountry)country).setTheMostSnowingMonth(Months.valueOf(countryInner.getTextContent()));
-                                }
-                                break;
-                            }
-
-                            case "polarnight": {
-                                if(country instanceof ColdCountry) {
-                                    ((ColdCountry) country).setPolarNight("polarnight".equals(countryInner.getTextContent()));
-                                }
-                                break;
-                            }
-
-                            case "average":{
-                                if(country instanceof HotCountry){
-                                    ((HotCountry)country).setAverageTemp(Integer.parseInt(countryInner.getTextContent()));
-                                }else {
-                                    ((ColdCountry)country).setAverageSnowLevel(Integer.parseInt(countryInner.getTextContent()));
-                                }
-                                break;
-                            }
-
-                            case "cities": {
-                                country.setCities(new ArrayList<>());
-
-                                NodeList xmlCities = ((Element) countryInner).getElementsByTagName("city");
-
-                                for (int k = 0; k < xmlCities.getLength(); k++) {
-                                    Element xmlCity = (Element) xmlCities.item(k);
-
-                                    City city = new City();
-                                    country.getCities().add(city);
-
-                                    city.setName(xmlCity.getElementsByTagName("name").item(0).getTextContent());
-                                    city.setPopulation(Integer.parseInt(xmlCity.getElementsByTagName("population").item(0).getTextContent()));
-                                    city.setCapital("capital".equals(xmlCity.getElementsByTagName("iscapital").item(0).getTextContent()));
-                                }
-                                break;
-                            }
-                        }
-
-                    }
+                    fillCountryFields(country, countryInnerTags);
                 }
             }
-
         }
         return parsedCountries;
     }
+
+    private void fillCountryFields(BaseCountry country, NodeList countryInnerTags){
+        for (int j = 1; j < countryInnerTags.getLength(); j++) {
+            Node countryInner = countryInnerTags.item(j);
+            switch (countryInner.getNodeName()){
+                case "name": {
+                    country.setName(countryInner.getTextContent());
+                    break;
+                }
+                case "language": {
+                    country.setLanguage(countryInner.getTextContent());
+                    break;
+                }
+                case "telephoneCode": {
+                    country.setTelephoneCode(countryInner.getTextContent());
+                    break;
+                }
+                case "month": {
+                    if(country instanceof HotCountry){
+                        ((HotCountry)country).setHottestMonth(Months.valueOf(countryInner.getTextContent()));
+                    }else {
+                        ((ColdCountry)country).setTheMostSnowingMonth(Months.valueOf(countryInner.getTextContent()));
+                    }
+                    break;
+                }
+                case "polarnight": {
+                    if(country instanceof ColdCountry) {
+                        ((ColdCountry) country).setPolarNight("polarnight".equals(countryInner.getTextContent()));
+                    }
+                    break;
+                }
+                case "average":{
+                    if(country instanceof HotCountry){
+                        ((HotCountry)country).setAverageTemp(Integer.parseInt(countryInner.getTextContent()));
+                    }else {
+                        ((ColdCountry)country).setAverageSnowLevel(Integer.parseInt(countryInner.getTextContent()));
+                    }
+                    break;
+                }
+                case "cities": {
+                    addCitiesToCountry(country, countryInner);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void addCitiesToCountry(BaseCountry country, Node countryInner){
+        country.setCities(new ArrayList<>());
+
+        NodeList xmlCities = ((Element) countryInner).getElementsByTagName("city");
+
+        for (int k = 0; k < xmlCities.getLength(); k++) {
+            Element xmlCity = (Element) xmlCities.item(k);
+
+            City city = new City();
+            country.getCities().add(city);
+
+            city.setName(xmlCity.getElementsByTagName("name").item(0).getTextContent());
+            city.setPopulation(Integer.parseInt(xmlCity.getElementsByTagName("population").item(0).getTextContent()));
+            city.setCapital("capital".equals(xmlCity.getElementsByTagName("iscapital").item(0).getTextContent()));
+        }
+    }
 }
+
+
